5eed827ad2cef92a4977dbeeac3b051fa3dc9d82
vickyungemach <vicky.ungemach95@gmail.com>
2023-08-12 10:48:57 -0700

    -m

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import Image from 'next/image'
+import squirrel from '@/public/images/squirrel.png'
 
 const Header = ({ }) => {
 
     return (
-        <div className='w-full bg-orange-300 text-center text-white font-bold tracking-widest py-4 mb-4'>
-            <span>SQUIRRELED</span>
+        <div className='w-full bg-orange-300 text-center text-white font-bold tracking-widest py-4 px-16 mb-4 flex items-center'>
+            <div id="logo" className='h-12 w-12 relative mr-2'>
+                <Image src={squirrel} fill style={{ objectFit: 'contain' }} />
+            </div>
+            <span className='logo text-4xl'>Squirreled</span>
+
         </div>
     )
 }

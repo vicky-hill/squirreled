701b2745b422e59220c3b2cc9cc2448dab8859f6
vickyexcersys <vicky.ungemach95@gmail.com>
2023-11-03 07:03:27 -0700

    add image kit upload to add item form

--- components/add/AddItem.js
+++ components/add/AddItem.js
@@ -1,8 +1,11 @@
-import { useState } from "react"
+import { useContext, useState, useEffect } from "react"
 import Button from "../elements/Button"
 import Modal from "../elements/Modal"
 import Form, { TextInput } from "../form/Form"
 import * as Yup from "yup"
+import CreatableSelect from 'react-select/creatable'
+import LocationContext from "@/context/LocationContext"
+import { IKContext, IKUpload } from 'imagekitio-react'
 
 const validationSchema = Yup.object({
     name: Yup.string().required('Please type something'),
@@ -11,11 +14,27 @@ const validationSchema = Yup.object({
 
 const AddItem = ({ }) => {
     const [modal, setModal] = useState(false);
+    const [options, setOptions] = useState([]);
     const [form, setForm] = useState(true);
+    const [file, setFile] = useState(null);
     const [values, setValues] = useState({
         name: '',
         description: ''
     });
+
+    const { locations } = useContext(LocationContext);
+
+    useEffect(() => {
+        if (locations) {
+            setOptions(locations.map(location => (
+                { value: location._id, label: location.description }
+            )));
+        }
+    }, [locations]);
+
+    const handleUpload = (e) => {
+        setFile(e.target.files[0])
+    }
 
     const onSubmit = async () => {
         try {
@@ -27,6 +46,16 @@ const AddItem = ({ }) => {
         }
     };
 
+    const onCreate = (e) => {
+        // const event = { name: e, year: 2023 }
+        // options.push({ value: event.name, label: `${event.name} ${event.year}` })
+        console.log('create', e)
+    }
+
+    const onChange = (e) => {
+        console.log('change', e)
+    }
+
     const resetForm = () => {
         setTimeout(() => {
             setForm(false);
@@ -34,6 +63,42 @@ const AddItem = ({ }) => {
         setTimeout(() => {
             setForm(true);
         }, 501);
+    };
+
+    const onUploadStart = (evt) => {
+        console.log('Started', evt);
+      };
+    
+      const onUploadProgress = (evt) => {
+        console.log('Progress: ', evt);
+      };
+    
+      const onError = (err) => {
+        console.log('Error');
+        console.log(err);
+      };
+    
+      const onSuccess = (res) => {
+        console.log('Success');
+        console.log(res);
+      };
+
+    const authenticator = async () => {
+        try {
+    
+            const response = await fetch('https://masterapi.pro/api/minite/image/imagekit');
+    
+            if (!response.ok) {
+                const errorText = await response.text();
+                throw new Error(`Request failed with status ${response.status}: ${errorText}`);
+            }
+    
+            const data = await response.json();
+            const { signature, expire, token } = data;
+            return { signature, expire, token };
+        } catch (error) {
+            throw new Error(`Authentication request failed: ${error.message}`);
+        }
     };
 
     return (
@@ -52,7 +117,32 @@ const AddItem = ({ }) => {
                             >
                                 <TextInput name="name" />
                                 <TextInput name="description" />
-                                <button type="submit">Add new item</button>
+
+                                <label className='form__group-label'>Location</label>
+                                <CreatableSelect
+                                    isClearable options={options}
+                                    classNamePrefix='location-select'
+                                    onCreateOption={onCreate}
+                                    onChange={onChange}
+                                    placeholder="Select location"
+                                />
+
+                                <IKContext
+                                    publicKey={process.env.IK_PUBLIC_KEY}
+                                    urlEndpoint='https://ik.imagekit.io/minite'
+                                    authenticator={authenticator}
+                                >
+                                    <IKUpload
+                                        fileName="img.jpg"
+                                        folder={"/Squirreled"}
+                                        onError={onError}
+                                        onSuccess={onSuccess}
+                                        onUploadStart={onUploadStart}
+                                        onUploadProgress={onUploadProgress}
+                                    />
+                                </IKContext>
+
+                                <button className="mt-6" type="submit">Add new item</button>
                             </Form>
                         )
                     }
--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const nextConfig = {
   env: {
     DB_URI: 'mongodb+srv://vicky:sweetwhitecat2018@cluster0.5hpkrft.mongodb.net/?retryWrites=true&w=majority',
     BASE_URL: 'http://localhost:3000/api',
-    JWT_SECRET: 'WhiteCat2018'
+    JWT_SECRET: 'WhiteCat2018',
+    IK_PUBLIC_KEY: 'public_nC0iM2AKxJN+PFW7nO2D9lhdE94='
   },
   images: {
     domains: ['ik.imagekit.io']
